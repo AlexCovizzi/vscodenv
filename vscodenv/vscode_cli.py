07415580638f7d31f8e9e2982275af485055f982
Alex Covizzi <alexcovizzi@gmail.com>
2018-04-27 19:26:42 +0200

    added print error

--- vscodenv/vscode_cli.py
+++ vscodenv/vscode_cli.py
@@ -5,17 +5,19 @@ from subprocess import Popen, PIPE
 from sys import stdout, stderr
 
 def code_install(extension, extensions_dir):
-    _code_execute("--extensions-dir", extensions_dir, "--install-extension", extension)
+    err = _code_execute("--extensions-dir", extensions_dir, "--install-extension", extension)
+    print(err)
 
 def code_uninstall(extension, extensions_dir):
-    _code_execute("--extensions-dir", extensions_dir, "--uninstall-extension", extension)
+    err = _code_execute("--extensions-dir", extensions_dir, "--uninstall-extension", extension)
+    print(err)
 
-def code_open(extensions_dir):
-    _code_execute("--extensions-dir", extensions_dir)
+def code_open(path, extensions_dir):
+    _code_execute(path, "--extensions-dir", extensions_dir)
 
 def _code_execute(*args):
     command = list(args)
     command.insert(0, "code")
     proc = Popen(command, stdout=stdout, stderr=PIPE, universal_newlines=True)
     _,err = proc.communicate()
-    return err
+    return err.rstrip("\n")
