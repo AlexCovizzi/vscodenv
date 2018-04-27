d453c013b8a96f2e965715356e89f0139470236f
Alex Covizzi <alexcovizzi@gmail.com>
2018-04-27 19:27:31 +0200

    added .obsolete check when listing extensions installed

--- vscodenv/vscode_extensions.py
+++ vscodenv/vscode_extensions.py
@@ -3,7 +3,8 @@ Module to search, install, uninstall vscode extensions
 '''
 import os
 from . import vscode_cli
-from .utils import get_global_extensions_dir
+from .utils import get_global_extensions_dir, extension_base_name
+import json
 
 
 def install_extension(extension, extensions_dir):
@@ -14,7 +15,7 @@ def install_extension(extension, extensions_dir):
         local_extension_path = os.path.join(extensions_dir, installed_extension)
         try:
             os.symlink(installed_extension_path, local_extension_path)
-        except IOError as e:
+        except IOError:
             pass
     else:
         vscode_cli.code_install(extension, extensions_dir)
@@ -47,12 +48,31 @@ def get_extensions(extensions_dir):
         candidate_extensions = os.listdir(extensions_dir)
     except IOError:
         pass
+    
+    obsolete_extensions = parse_dot_obsolete(extensions_dir)
     for candidate in candidate_extensions:
         candidate_path = os.path.join(extensions_dir, candidate)
         # an extension MUST be a directory
         if os.path.isdir(candidate_path):
             # an extension MUST have a 'package.json' file
             package_json_path = os.path.join(candidate_path, 'package.json')
-            if os.path.exists(package_json_path):
+            package_json_exists = os.path.exists(package_json_path)
+            # an extension must not be in .obsolete file
+            extension_in_dot_obsolete = (candidate in obsolete_extensions)
+            if package_json_exists and not extension_in_dot_obsolete:
                 extensions.append(candidate)
     return extensions
+
+def parse_dot_obsolete(extensions_dir):
+    dot_obsolete_path = os.path.join(extensions_dir, '.obsolete')
+    obsolete_extensions = []
+    if os.path.exists(dot_obsolete_path):
+        try:
+            data = json.load(open(dot_obsolete_path))
+            obsolete_extensions = list(data.keys())
+        except IOError:
+            pass
+        except json.JSONDecodeError:
+            pass
+
+    return obsolete_extensions
