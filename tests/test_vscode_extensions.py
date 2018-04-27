21c16b7ea48566f73d8027f023fcf8f9aed1f173
Alex Covizzi <alexcovizzi@gmail.com>
2018-04-27 19:29:30 +0200

    added stub for tests for vscode_extensions module

---
+++ tests/test_vscode_extensions.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestVscodeExtensions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
